00f247672f271ab4cf2f33667c041666550d24fa
alzaabi555 <157557210+alzaabi555@users.noreply.github.com>
2026-03-21 08:25:56 +0400

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.alzaabi.rased.student',
+  appName: 'Rased Student',
+  webDir: 'dist'
+};
+
+export default config;
