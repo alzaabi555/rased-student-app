18b1e2991f60e0f267100cac7fcd2e3983231dc3
alzaabi555 <157557210+alzaabi555@users.noreply.github.com>
2026-04-02 12:14:05 +0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // 👈 هذا هو السطر السحري الذي تمت إضافته
+  base: '/https://github.com/alzaabi555/rased-student-app/', 
 })
