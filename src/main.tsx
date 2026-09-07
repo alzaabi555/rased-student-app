38f040b53df4aabea723c04310e5e417cecdc3f7
alzaabi555 <157557210+alzaabi555@users.noreply.github.com>
2026-09-07 05:34:15 +0400

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css' // 👈 هذا هو السطر المفقود الذي سيعيد الحياة للألوان!
+import './index.css'
 import App from './App'
 
 createRoot(document.getElementById('root')!).render(
